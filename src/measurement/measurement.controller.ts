6d6789a8dfd97a37957f8b358ba65527ab3ca13a
techrook <mondayitohowo3@gmail.com>
2023-07-21 00:20:53 +0100

    add measurement functon store image in cloud

--- src/measurement/measurement.controller.ts
+++ src/measurement/measurement.controller.ts
@@ -1,16 +1,66 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { MeasurementService } from './measurement.service';
 import { CreateMeasurementDto } from './dto';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @UseGuards(JwtGuard)
 @Controller('measurement')
 export class MeasurementController {
-    constructor(private measurementService:MeasurementService){}
+  constructor(private measurementService: MeasurementService) {}
 
-    @Post('addmeasurement')
-    addMeasurement(@GetUser('id') userId: number, @Body() dto:CreateMeasurementDto){
-        return this.measurementService.addMeasurement(userId, dto)
-    }
+  @Post('addmeasurement')
+  @UseInterceptors(FileInterceptor('file'))
+  async addMeasurement(
+    @GetUser('id') userId: number,
+    @Body('gender') gender: string,
+    @Body('cloth_Owner_name') cloth_Owner_name: string,
+    @Body('chest') chest: number,
+    @Body('Waist') Waist: number,
+    @Body('Hips') Hips: number,
+    @Body('neck_to_waist') neck_to_waist: number,
+    @Body('waist_down_to_desired_lenght') waist_down_to_desired_lenght: number,
+    @Body('laps') laps: number,
+    @Body('wrist') wrist: number,
+    @Body('ankle') ankle: number,
+    @Body('shoulders') shoulders: number,
+    @Body('neck') neck: number,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.measurementService.addMeasurement(
+      file,
+      userId,
+      gender,
+      cloth_Owner_name,
+      chest,
+      Waist,
+      Hips,
+      neck_to_waist,
+      waist_down_to_desired_lenght,
+      laps,
+      wrist,
+      ankle,
+      shoulders,
+      neck
+    );
+  }
+
+  //   @Post('upload')
+  //   @UseInterceptors(FileInterceptor('file')) // 'file' is the field name for the uploaded file in the request
+  //   async uploadImage(@UploadedFile() file: Express.Multer.File) {
+  //     try {
+  //       const imageUrl = await this.measurementService.uploadImage(file);
+  //       return { message: 'Image uploaded successfully', imageUrl };
+  //     } catch (error) {
+  //       throw new HttpException('Failed to upload image', HttpStatus.INTERNAL_SERVER_ERROR);
+  //     }
+  //   }
 }
--- src/measurement/measurement.service.ts
+++ src/measurement/measurement.service.ts
@@ -1,28 +1,78 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateMeasurementDto } from './dto';
-
+import { Storage } from '@google-cloud/storage';
+import multer from 'multer';
+import * as path from 'path';
 @Injectable()
 export class MeasurementService {
-    constructor(
-        private prisma: PrismaService,
+  keyFilePath = path.resolve(__dirname, 'digitalmeasurement.json');
+  constructor(private prisma: PrismaService) {}
+  async addMeasurement(
+    file: Express.Multer.File,
+    userid: number,
+    gender,
+    cloth_Owner_name,
+    chest,
+    Waist,
+    Hips,
+    neck_to_waist,
+    waist_down_to_desired_lenght,
+    laps,
+    wrist,
+    ankle,
+    shoulders,
+    neck,
+  ) {
+    try {
+      const storage = new Storage({
+        keyFilename: this.keyFilePath,
+      });
 
-      ){}
-        async addMeasurement(userid:number, dto:CreateMeasurementDto){
-            try {
-                const measurement = await this.prisma.measurement.create({
-                   data: {
-                    gender: dto.gender,
-                    cloth_Owner_name: dto.cloth_Owner_name,
-                    userId: userid,
-                    ...dto
-                   },
-                })
-                if(!measurement) throw new HttpException("bad request", HttpStatus.BAD_REQUEST)
-                return measurement
-            } catch (error) {
-                throw new HttpException('server error', HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        
+      const digitameasurementbucket = 'digitameasurementbucket'; // Replace with your actual bucket name
+      const bucket = storage.bucket(digitameasurementbucket);
+      const uniqueFilename = `${Date.now()}-${file.originalname}`;
+      const fileUpload = bucket.file(uniqueFilename);
+      const stream = fileUpload.createWriteStream({
+        metadata: {
+          contentType: file.mimetype,
+        },
+      });
+
+      stream.on('error', (err) => {
+        console.log(err);
+      });
+
+      stream.on('finish',async () => {
+        // Save the image URL to the database after successful upload
+       console.log('finished image upload')
+      });
+      stream.end(file.buffer);
+      const imageUrl = `https://storage.googleapis.com/${digitameasurementbucket}/${uniqueFilename}`;
+      const measurement = await this.prisma.measurement.create({
+        data: {
+          gender: gender,
+          cloth_Owner_name: cloth_Owner_name,
+          chest: Number(chest),
+          Waist: Number(Waist),
+          Hips: Number(Hips),
+          neck_to_waist: Number(neck_to_waist),
+          waist_down_to_desired_lenght: Number(waist_down_to_desired_lenght),
+          laps: Number(laps),
+          wrist: Number(wrist),
+          ankle: Number(ankle),
+          shoulders: Number(shoulders),
+          neck: Number(neck),
+          userId: userid,
+          dress_design: imageUrl,
+        },
+      });
+      console.log(imageUrl);
+      if (!measurement)
+        throw new HttpException('bad request', HttpStatus.BAD_REQUEST);
+      return measurement;
+    } catch (error) {
+      throw new HttpException('Server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
