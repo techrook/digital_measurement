be520949316725848064ef8f5f5b5457f3df550d
techrook <mondayitohowo3@gmail.com>
2024-02-27 20:03:09 +0100

    added enviroment sample and rewrote prisma service

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  await app.listen(3333);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
+  console.log(`App listening on http://localhost:${PORT}`);
 }
 bootstrap();
